011f0ca510009acc5ee8d48197cb90015abce391
Nam <ngkimnhatnam@gmail.com>
2021-05-09 12:43:04 +0300

    setup backend dependencies, linting & typescript initiation

---
+++ server/src/server.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import express from 'express';
+import loaders from './loaders/index';
+import httpServer from 'http';
+
+const port = process.env.PORT || 8080;
+
+const startServer = async () => {
+  const application = express();
+
+  const app = await loaders({ expressApp: application });
+
+  const server = httpServer.createServer(app);
+
+  const io = require('socket.io')(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
+  });
+  // @ts-ignore
+  io.on('connection', (socket) => {
+    console.log('Connected to server');
+    socket.on('send-order', (order: any) => {
+      socket.local.emit('new-order', order);
+    });
+    socket.on('remove-order', (id: any) => {
+      socket.local.emit('delete-order', id);
+    });
+  });
+  server.listen(port);
+  console.log('Server running at port ', port);
+};
+
+startServer();
